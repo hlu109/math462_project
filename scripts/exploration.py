3ba25737d50a1d31041b74fe0319488a913e6e03
Hannah Lu <hannahlu1207@yahoo.com>
2023-04-20 22:54:42 -0700

    initial exploration

--- scripts/exploration.py
+++ scripts/exploration.py
@@ -1,16 +1,22 @@
 from utils import *
 import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
-    monthly_data, yearly_data = load_dataset()
-    boroughs = monthly_data["area"].unique()
+
+def plot_var(df, var_name):
+    areas = df["area"].unique()
     fig, axs = plt.subplots(5, 9, sharex=True, sharey=True)
     axs = axs.reshape(-1)
-    start_date = sorted(monthly_data["date"])[0]
-    for borough, ax in zip(boroughs, axs):
-        borough_data = get_area(monthly_data, borough)
-        ax.plot(borough_data["date"] - start_date, borough_data["average_price"])
-        ax.set_title(borough)
+    start_date = sorted(df["date"])[0]
+    for area, ax in zip(areas, axs):
+        area_data = get_area(df, area)
+        ax.plot(area_data["date"] - start_date, area_data[var_name])
+        ax.set_title(area)
     fig.tight_layout()
     plt.show()
-    fig.savefig("../plots/avg_price_vs_time_by_borough.png", bbox_inches="tight")
+    fig.savefig("../plots/{}_vs_time_by_area.png".format(var_name),
+                bbox_inches="tight")
+
+
+if __name__ == "__main__":
+    monthly_data, yearly_data = load_dataset()
+    plot_var(monthly_data, "average_price")
