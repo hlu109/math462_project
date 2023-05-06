0d7d5ccb45afac5fca61f0234cb55ae8c178bff0
Hannah Lu <hannahlu1207@yahoo.com>
2023-05-05 21:26:14 -0700

    add LSTM model and results

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Chris Herrera and Hannah Lu
 https://www.kaggle.com/datasets/justinas/housing-in-london?resource=download
 
 # Methods
+(Linear Regression)
 RNNs, specifically  LSTMs
 Reservoir computing
 Hidden Markov Model
--- scripts/utils.py
+++ scripts/utils.py
@@ -328,7 +328,6 @@ def interpolate_yearly(monthly_data, yearly_resampled, col, interpolate=True):
     return monthly_data
 
 
-
 def plot_multi_acf(data, lags, titles, suptitle='', ylim=None, partial=False):
     """ Plot autocorrelation at a variety of frequencies. 
     
@@ -344,9 +343,25 @@ def plot_multi_acf(data, lags, titles, suptitle='', ylim=None, partial=False):
         ax = [ax]
     acf_func = smt.graphics.plot_pacf if partial else smt.graphics.plot_acf
     for idx, (lag, title) in enumerate(zip(lags, titles)):
-        ax[idx] = acf_func(data, lags=lag, ax=ax[idx], title=title);
+        ax[idx] = acf_func(data, lags=lag, ax=ax[idx], title=title)
         if ylim is not None:
             ax[idx].set_ylim(ylim)
 
     fig.tight_layout()
 
+
+def create_windowed_dataset(dataset, look_back=1, look_forward=1):
+    """ creates new time series data with past datapoints as input features. 
+    
+    Copied from https://machinelearningmastery.com/time-series-prediction-lstm-recurrent-neural-networks-python-keras/
+    
+        Args:
+            dataset: time series array
+            look_back (int): num of previous time steps to use as input variables to predict the next time period
+    """
+    dataX, dataY = [], []
+    for i in range(len(dataset) - look_back - look_forward):
+        a = dataset[i:(i + look_back), 0]
+        dataX.append(a)
+        dataY.append(dataset[(i + look_back):(i + look_back + look_forward), 0])
+    return np.array(dataX), np.array(dataY)
